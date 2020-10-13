b2817c9ec30c5466a059e0c91336ed040f486610
Ilnar <ilnarshan@gmail.com>
2020-10-13 17:24:32 +0400

    17. Оживляем интерактивные элементы

--- js/main.js
+++ js/main.js
@@ -1,23 +1,54 @@
 const  contactUs = document.querySelector(".about__contacts-link");
 const contactUsPopup = document.querySelector(".modal__write-us");
 const contactUsClose = contactUsPopup.querySelector(".modal-close");
+const contactUsForm = contactUsPopup.querySelector("form");
+const userName = contactUsPopup.querySelector("#modalUserName");
+const userMail = contactUsPopup.querySelector("#modalEmail");
+const userText = contactUsPopup.querySelector(".modal__box-textarea");
+
+
+const isStorageSupport = true;
+const storage = "";
+
+
 
 contactUs.addEventListener("click", function () {
-  // evt.preventDefault();
-  // console.log(" Клик по кнопке ")
   contactUsPopup.classList.add("modal__write-us--active");
+
+  if (storage) {
+    userName.value = storage;
+    userMail.value = storage;
+    // userText.focus();
+  } 
+  else {
+    userName.focus();
+  } 
 });
 
 
 contactUsClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactUsPopup.classList.remove("modal__write-us--active");
+  contactUsPopup.classList.remove("modal-error");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (contactUsPopup.classList.contains("modal__write-us--active")) {
       contactUsPopup.classList.remove("modal__write-us--active");
+      contactUsPopup.classList.remove("modal-error");
+    }
+  }
+});
+
+contactUsForm.addEventListener("submit", function (evt) {
+    if (!userName.value || !userMail.value) {
+      evt.preventDefault();
+      contactUsPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+    localStorage.setItem("userName", userName.value);
+    localStorage.setItem("userMail", userMail.value);
     }
   }
 });
